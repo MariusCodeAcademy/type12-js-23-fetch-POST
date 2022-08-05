5c1d7a096fdd43a845725fa38d23df87ce38f653
Marius <marius@example.com>
2022-08-05 12:00:50 +0300

    rest login

--- js/main.js
+++ js/main.js
@@ -15,4 +15,31 @@ formEl.addEventListener('submit', formSubmitHandler);
 function formSubmitHandler(event) {
   event.preventDefault();
   // surikti formos inputus i viena objekta ir iskonsolinti
+  const userLoginObj = {
+    username: formEl.elements.login.value,
+    password: formEl.elements.password.value,
+  };
+  console.log('userLoginObj ===', userLoginObj);
+  sendLoginFetch(userLoginObj);
+}
+
+async function sendLoginFetch(userDataObj) {
+  const resp = await fetch('https://fakestoreapi.com/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(userDataObj),
+  });
+  console.log('resp ===', resp);
+  if (resp.ok) {
+    const dataInJs = await resp.json();
+    console.log('dataInJs ===', dataInJs);
+    alert('login success');
+    // redirect
+    window.location.href = 'user-page.html';
+    return;
+  }
+  const errorResponseMessage = await resp.text();
+  alert(errorResponseMessage);
 }
