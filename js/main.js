25cce0a0b9eff0d332d45e13358be6c8b60707b1
Marius <marius@example.com>
2022-08-05 11:17:21 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('main.js');
+
+//  nusitaikom
+const formEl = document.forms[0];
+const loginEl = document.getElementById('login');
+const passwordEl = document.getElementById('password');
+
+formEl.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler() {}
